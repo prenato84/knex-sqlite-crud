a3604f3ccd8174e5cc5cc0f8ba15cb5d540cc954
Paulo Castro <prenato84@gmail.com>
2020-05-07 17:48:57 -0300

    add  migration to alter table users

--- src/database/migrations/20200507114935_create_table_users.js
+++ src/database/migrations/20200507114935_create_table_users.js
@@ -3,6 +3,7 @@ exports.up = (knex) =>
     table.increments("id");
     table.text("username").unique().notNullable();
 
+    // table.timestamps(true, true);
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
