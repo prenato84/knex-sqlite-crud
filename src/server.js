dcc5218bb2d6155b9766a87078dc7f0ad1b9c040
Paulo Castro <prenato84@gmail.com>
2020-05-07 14:51:36 -0300

    add update and delete user routes

--- src/server.js
+++ src/server.js
@@ -6,4 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+// catch all
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
+
 app.listen(3333, () => console.log("Server is running"));
