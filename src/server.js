2db583749368e87e3dbd40d01a333e85adc44b8a
Paulo Castro <prenato84@gmail.com>
2020-05-07 14:17:22 -0300

    fix database index exports

--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const knex = require("./database");
+
 const app = express();
+
+app.get("/users", (req, res) =>
+  knex("users").then((results) => res.json(results))
+);
 
 app.listen(3333, () => console.log("Server is running"));
