243df7370875d807164eeea5c7842b741c77e4ab
Paulo Castro <prenato84@gmail.com>
2020-05-07 14:28:14 -0300

    refactor server, routes and create UserController

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const knex = require("./database");
+const routes = require("./routes");
 
 const app = express();
 
-app.get("/users", (req, res) =>
-  knex("users").then((results) => res.json(results))
-);
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running"));
